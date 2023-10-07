8fc3b7d7e8712e1f2d5d70b8f8a7bf50f3f9fbf1
Kristian Lewis <kristianedwardlewis@gmail.com>
2023-10-07 13:12:35 -0400

    Change user table. Change profilePicture to VARCHAR(5). Give default values to profilePicture weight age. Stop setting those values with insert

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 /*
 TODO
 
--this can be cleaned up better
-
+-make multiple options
+    -one option would be to just drop and reacreate tables
+    -another option would be to drop and create tables and also add 10 accounts and fill random food for that day
+        -it should also store their IDs in a json file
+    -the thir option should use the IDs store in the json file to add random food to those Ids for that day
 
 */
 
@@ -28,6 +31,7 @@ const connection = mysql.createPool({
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
+//does connectino .execute have a different way of intepreting things from connection.query
 
 async function dropTables(){
     console.log("drop tables")
@@ -38,7 +42,6 @@ async function dropTables(){
     .catch(error => {
         console.log("loggedMealItems doesnt exist")
     })
-
     await connection.query('DROP TABLE users')
     .then(res => {
         console.log("users Dropped")    
@@ -46,7 +49,6 @@ async function dropTables(){
     .catch(error => {
         console.log("users doesnt exist")
     })
-
     return(true)
 }
 
@@ -54,14 +56,7 @@ async function createTables () {
 
     console.log("create tables")
     await connection.query(
-        `CREATE TABLE users(
-            userID VARCHAR(36) PRIMARY KEY, 
-            username VARCHAR(16) NOT NULL UNIQUE, 
-            password VARCHAR(60), 
-            name VARCHAR(16), 
-            profilePicture BOOL,
-            weight INT, 
-            age INT)`)
+        `CREATE TABLE users( userID VARCHAR(36) PRIMARY KEY, username VARCHAR(16) NOT NULL UNIQUE, password VARCHAR(60) NOT NULL, name VARCHAR(16) NOT NULL, profilePicture VARCHAR(5) DEFAULT 'none', weight INT DEFAULT 0, age INT DEFAULT 0)`)
     .then(res => {
         console.log("users made")
     })
@@ -70,9 +65,12 @@ async function createTables () {
     })
 
 
-    await connection.query('CREATE TABLE loggedMealItems(loggedID VARCHAR(36) PRIMARY KEY, foodItemID VARCHAR(36), userID VARCHAR(36), day DATE, amount INT, meal INT, FOREIGN KEY (foodItemID) REFERENCES foodItems(foodItemID), FOREIGN KEY (userID) REFERENCES users(userID))')
+    await connection.query(`CREATE TABLE loggedMealItems(loggedID VARCHAR(36) PRIMARY KEY, foodItemID VARCHAR(36), userID VARCHAR(36), day DATE, amount INT, meal INT, FOREIGN KEY (foodItemID) REFERENCES foodItems(foodItemID), FOREIGN KEY (userID) REFERENCES users(userID))`)
+        .then(res => {
+            console.log("loggedMealItems made")
+        })
     .catch(error => {
-        console.log("couldnt create logged items")
+        console.log("Couldn't create logged items")
         throw error
     })
 
@@ -89,19 +87,38 @@ async function insertData () {
         const username = `'username${i}'`
         const password = `password${i}`
         const name = `'Name ${i}'`;
-        const weight = Math.floor(Math.random() * (200 - 100) + 100);
-        const age = Math.floor(Math.random() * (100 - 18) + 18);
+        
+        /*const weight = Math.floor(Math.random() * (200 - 100) + 100);
+        const age = Math.floor(Math.random() * (100 - 18) + 18);*/
 
         const hash = bcrypt.hashSync(password, saltRounds);
 
+        /* old version
+        await connection.query(`INSERT INTO users(
+            userID, 
+            username, 
+            password, 
+            name, 
+            profilePicture, 
+            weight, 
+            age) 
+            VALUES (${userID}, ${username}, '${hash}', ${name}, FALSE, ${weight}, ${age})`)
+        .catch(error => {
+            console.log(error)
+        })
+
+
+        */
+
+
         //awaiting here for now, should probably do a propmise.all again
-        await connection.query(`INSERT INTO users(userID, username, password, name, profilePicture, weight, age) VALUES (${userID}, ${username}, '${hash}', ${name}, FALSE, ${weight}, ${age})`)
+        await connection.execute(`INSERT INTO users(userID, username, password, name) VALUES (${userID}, ${username}, '${hash}', ${name})`)
         .catch(error => {
             console.log(error)
         })
     }
 
-    const foodItemIDList = await connection.query('SELECT foodItemID FROM foodItems ORDER BY RAND() LIMIT 0,10')
+    const foodItemIDList = await connection.execute('SELECT foodItemID FROM foodItems ORDER BY RAND() LIMIT 0,10')
     .then(res => {
         return res[0]
     })
@@ -112,11 +129,9 @@ async function insertData () {
     for (let i = 1; i < 11; i++)
     {
         const userID = userIDList[i-1];
-    
         for (let j = 0; j < 4; j++)
         {
             const meal = j;
-    
             for (let k = 0; k < 3; k++)
             {
                 const loggedID = `'${uuid.v4()}'`;
@@ -124,19 +139,15 @@ async function insertData () {
                 const amount = Math.floor(Math.random() * (5) + 1);
                 //const meal = Math.floor(Math.random() * (4));
     
-                await connection.query(`INSERT INTO loggedMealItems(loggedID, foodItemID, userID, day, amount, meal) VALUES (${loggedID}, ${foodItemID}, ${userID}, CURDATE(), ${amount}, ${meal})`)
+                await connection.execute(`INSERT INTO loggedMealItems(loggedID, foodItemID, userID, day, amount, meal) VALUES (${loggedID}, ${foodItemID}, ${userID}, CURDATE(), ${amount}, ${meal})`)
                 .then(res => {
                 })
                 .catch(error =>{
                     console.log(error)
                 })
-    
             }
-    
         }
-    
     }
- 
     return true
 }
 
@@ -147,6 +158,7 @@ dropTables()
         insertData()
         .then(res => {
             connection.end()
+            console.log("End")
         })
     })
 })
